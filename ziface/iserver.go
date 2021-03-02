30078fa80ac4856922f6f9919c2db93e5d1c45d1
taoey <hwt8080@163.com>
2021-03-02 22:47:57 +0800

    FEATURE 新增基础server接口及相关实现

---
+++ ziface/iserver.go
@@ -0,0 +1,7 @@
+package ziface
+
+type IServer interface {
+	Start()  // 启动服务器
+	Server() // 运行服务器
+	Stop()   // 停止服务器
+}
---
+++ znet/server.go
@@ -0,0 +1,20 @@
+package znet
+
+type Server struct {
+	Name      string // 服务器名称
+	IPVersion string // 服务器IP版本
+	IP        string // 服务器监听IP
+	Port      int    // 监听端口号
+}
+
+func (s *Server) Start() {
+
+}
+
+func (s *Server) Server() {
+
+}
+
+func (s *Server) Stop() {
+
+}
